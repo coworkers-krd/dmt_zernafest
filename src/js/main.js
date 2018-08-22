c518b65dbec684c4c9b10960ac4d0c2b2d0b6da7
spureful <rw1zz@yzandex.ru>
2018-08-23 01:29:19 +0300

    made adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -7,4 +7,69 @@ function whereWillSlider() {
 
 whereWillSlider();
 
+// initMap() - функция инициализации карты
+function initMap() {
+	
+	var centerLatLng = new google.maps.LatLng(55.659573, 36.815790);
+	
+	var mapOptions = {
+		center: centerLatLng,
+		zoom: 17             
+	};
+	
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+}
+// Ждем полной загрузки страницы, после этого запускаем initMap()
+google.maps.event.addDomListener(window, "load", initMap);
 
+
+//OPEN HAMBURGER MENU
+
+function openMenu() {
+	const openBtn = $('.js-hamburger');
+	const openNav = $('.js-menu');
+	const menuItem = $('.menu__link');
+	
+	openBtn.on('click', function() {
+		
+		openNav.slideToggle();
+		
+	});
+	
+	menuItem.on('click', function() {
+		if(openNav.css('display', 'block')) {
+		openNav.slideUp();
+		}
+	})
+}
+
+openMenu();
+
+//ACTIVEMENU
+
+function activeMenu() {
+	
+}
+
+activeMenu();
+
+//SPEAKERS OPEN BUTTON
+
+function openElseSpeakers() {
+	const elseBtn = $('.js-speakers-btn');
+	
+	elseBtn.on('click', function() {
+		elseBtn.css('display', 'none');
+		$('.speakers__item_invisible').css('display', 'flex');
+	})
+}
+
+openElseSpeakers();
+
+//SPEAKERS MODAL
+
+function speakersModal() {
+	
+}
+
+speakersModal();
