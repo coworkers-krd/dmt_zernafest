21a0b07443ae38b20ca51aa976d41d7a799611ed
spureful <rw1zz@yzandex.ru>
2018-08-29 16:55:53 +0300

    еще правки от заказчика

--- src/js/main.js
+++ src/js/main.js
@@ -298,46 +298,94 @@ reviewsSlider();
 function reviewsContent() {
 	const slide = document.querySelector('.reviews__slider');
 	const reviewSlide = document.querySelectorAll('.reviews__slide');
-	const reviewContent = document.querySelectorAll('.reviews__content');
+	const reviewSlideImg = document.querySelectorAll('.reviews__slide-img');
+	const reviewContent = document.querySelector('.reviews__content');
+	
 	const bigImg = document.querySelector('.rewiews__big-img');
 
+	const name = document.querySelector('.reviews__name ');
+	const position = document.querySelector('.reviews__position');
+	const comment = document.querySelector('.reviews__comment');
 
 
+		const nameSlide = document.querySelectorAll('.reviews__slide-name ');
+	const positionSlide = document.querySelectorAll('.reviews__slide-position');
+	const commentSlide = document.querySelectorAll('.reviews__slide-comment');
+
+	let tr; 
+
+		
+			
+	for (let l = 0; l < reviewSlide.length; l++) {
 
 
+			
+
+				if (reviewSlide[l].classList.contains('slick-current')) {
+									
+
+										
+					let currentImg = reviewSlide[l].firstChild.getAttribute('src');
+					
+					bigImg.setAttribute('src', currentImg);
+					
+					name.textContent = reviewSlide[l].children[1].innerHTML;
+					position.textContent = reviewSlide[l].children[2].innerHTML;
+					comment.textContent = reviewSlide[l].children[3].innerHTML;
+					
+				}
+	}
+	
+	
 	slide.addEventListener('click', () => {
 
 		for (let i = 0; i < reviewSlide.length; i++) {
 
 
-			for (let l = 0; l < reviewSlide.length; l++) {
+			
 
-				if (reviewSlide[i].classList.contains('slick-center')) {
+				if (reviewSlide[i].classList.contains('slick-current')) {
+				
 					
-					
-					let currentSlide = reviewSlide[i].getAttribute('data-slideRew');
-					
-					let currentImg = reviewSlide[i].getAttribute('src');
-					
-					
-					reviewContent[l].classList.remove('reviews__content_active');
-					reviewContent[l].style.transform = 'translateX(100%)';
-					reviewContent[currentSlide].classList.add('reviews__content_active');
+				
+										
+					let currentImg = reviewSlide[i].firstChild.getAttribute('src');
 					
 					bigImg.setAttribute('src', currentImg);
+					
+					name.textContent = reviewSlide[i].children[1].innerHTML;
+					position.textContent = reviewSlide[i].children[2].innerHTML;
+					comment.textContent = reviewSlide[i].children[3].innerHTML;
+					
+					
+
+					reviewContent.style.transform = 'translateX(100%)';
+					tr = 100;
+					
+				
 
 					setTimeout(function RewTransl() {
+						
+						if(tr > 0 ) {
+							tr -= 10;
+						reviewContent.style.transform = `translateX(${tr}%)`;
+							
+							setTimeout(RewTransl, 50)
+						}
 
-						reviewContent[currentSlide].style.transform = 'translateX(0)';
-
-					}, 700);
+					}, 200);
 
 				}
 
-			}
+		
 
 		}
 	});
+	
+
+		
+	
+	
 };
 
 reviewsContent();
@@ -545,22 +593,22 @@ activeMenu();
 
 //PRELOADER 
 
-function preloader() {
-	document.body.onload = () => {
-
-		setTimeout(() => {
-			const preloader = document.querySelector('.preloader');
-			if (!preloader.classList.contains('preloader_done')) {
-				preloader.classList.add('preloader_done');
-			}
-
-		}, 1)
-
-
-	}
-}
-
-preloader();
+//function preloader() {
+//	document.body.onload = () => {
+//
+//		setTimeout(() => {
+//			const preloader = document.querySelector('.preloader');
+//			if (!preloader.classList.contains('preloader_done')) {
+//				preloader.classList.add('preloader_done');
+//			}
+//
+//		}, 1)
+//
+//
+//	}
+//}
+//
+//preloader();
 
 
 //модальное окно
